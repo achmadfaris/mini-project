49bb7dc0bc75ddddd9de04206d2add7b2c9aa61f
achmadfaris <ahmadfaris65@gmail.com>
2015-03-29 08:31:46 +0700

    Create home.php

---
+++ mpasik/home.php
@@ -0,0 +1,12 @@
+<?php include "header.php" ?>
+    	<div class="container">
+    		<div class="col-lg-6 text-center">
+    		<h1><p>Selamat Datang di Website AsikNginap.Com
+    		Cara Asik Cari Penginapan di Area Surabaya</p></h1>
+    		<h3><p>ayo buat akun sekarang dan rasakan keasikannya</p></h3>
+    		<button class="btn btn-primary btn-lg" type="submit">Daftar</button></div>
+    		</div>
+    		<div class="col-lg-6">
+    		</div>
+    	</div>
+    	<?php include "footer.php" ?>
