0f6abf9a876fc86a26c5fe36e355a236aa37753a
achmadfaris <ahmadfaris65@gmail.com>
2015-03-30 05:59:55 +0700

    upload file

---
+++ mpasik/carinap.php
@@ -0,0 +1,9 @@
+<?php include "header.php" ?>
+<hr>
+    	<div class="container">
+    		<ul class="nav nav-tabs">
+                <li role="presentation" class="active"><a href="#">Nginap Disini Aja</a></li>
+                <li role="presentation"><a href="#">Daftar Penginapan</a></li>
+            </ul>
+    	</div>
+    	<?php include "footer.php" ?>
