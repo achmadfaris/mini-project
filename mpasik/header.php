7e50ab33a8daeba0e7f248186930003640d8a45d
achmadfaris <ahmadfaris65@gmail.com>
2015-03-29 08:32:53 +0700

    Create header.php

---
+++ mpasik/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AsikNginap.Com</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+  	<!-- header -->
+    <div>
+    	<div class="container"> 
+    	<div class="col-lg-7"></div>
+    	<div class="col-lg-5">
+    	<form class="form-inline">
+    		<input type="text" class="form-control" placeholder="nama user">
+    		<input type="password" class="form-control" placeholder="kata sandi">
+   			<button class="btn btn-primary" type="submit">Masuk</button>
+    	</form>
+    		
+   		</div>    	
+    </div>
+    <!-- seacrch --> 
+    <div>
+    	<div class="container">
+		<form>
+			<div class="form-group">
+				<div class="col-lg-4"><input type="text" class="form-control" placeholder="kata pencarian"></div>
+				<div class="col-lg-3">
+					<select class="form-control">
+						<option value="1">Surabaya Pusat</option>
+						<option value="2">Surabaya Utara</option>
+						<option value="3">Surabaya Selatan</option>
+						<option value="4">Surabaya Barat</option>
+						<option value="5">Surabaya Timur</option>
+					</select></div>
+				<div class="col-lg-3">
+					<select class="form-control">
+						<option value="1">Hotel</option>
+						<option value="2">Wisma</option>
+						<option value="3">Rumah Kos</option>
+						<option value="4">Rumah Kontrak</option>
+						<option value="5">Apartemen</option>
+						<option value="6">Homestay</option>
+					</select></div>
+				<div class="col-lg-2">
+					<button class="btn btn-primary" type="submit">Cari</button></div>
+		</div> </form>  	
+    </div>
+    <!-- main home -->
+    <div>
