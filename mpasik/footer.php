3e47563f1b07cc80f39c8d8c3d9a74e44286eea6
achmadfaris <ahmadfaris65@gmail.com>
2015-03-29 08:33:36 +0700

    Create footer.php

---
+++ mpasik/footer.php
@@ -0,0 +1,15 @@
+   </div>
+   	<div>
+   		<div class="container"> 
+   		<div class="text-right"> 
+   			<h5><a class="text-right" href="#">FAQ</a>
+   			<a class="text-right" href="#">Tentang Kami</a></h5>
+   		</div>
+   			
+   	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+   
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
